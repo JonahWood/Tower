ebe0e78cd1cd857cf2b0a1a154aacce22e1aaef0
Jonah <jonah.osito@icloud.com>
2023-02-28 19:54:54 -0700

    init

---
+++ Tower.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'codethatworks.us.auth0.com'
+export const clientId = 'Ah38ojfR7RFYCSEwwDAdqkB2gaK4qD88'
+export const audience = 'https://CodeThatWorks.com'
